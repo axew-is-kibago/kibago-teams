de14adbefa674a6260a22d597fd0470e9ff5af47
Yanai1005 <tatsuya871_1005@icloud.com>
2023-09-17 22:57:35 +0900

    add:react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {BrowserRouter} from 'react-router-dom'
+import { createRoot } from 'react-dom/client'; 
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = document.getElementById('root');
+
+if (root) {
+const appRoot = createRoot(root);
+appRoot.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+} else {
+    console.error("Root element not found");
+}
