8ddc533abf2e71176f687a1a4625804934d80ecc
Yanai1005 <tatsuya871_1005@icloud.com>
2023-09-18 21:54:14 +0900

    add:api about register User

--- api/RegisterUsers/index.ts
+++ api/RegisterUsers/index.ts
@@ -1,17 +1,53 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import * as fs from 'fs';
+import * as mysql from 'mysql2/promise';
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+interface MegaInfo {
+    name: string;
+}
+async function registerThread(MegaInfo: MegaInfo): Promise<void> {
+    try {
+      const config = {
+        host: process.env["MYSQL_HOST"],
+        user: process.env["MYSQL_USER"],
+        password: process.env["MYSQL_PASSWORD"],
+        database: process.env["MYSQL_DB"],
+        port: 3306,
+        ssl: { ca: fs.readFileSync("DigiCertGlobalRootCA.crt.pem") }
+      };
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+      const conn = await mysql.createConnection(config);
+      const [rows] = await conn.execute(
+        'INSERT INTO Users (name) VALUES (?)',
+        [ MegaInfo.name]
+      );
+      conn.end();
 
+      console.log('テーブルにデータを登録しました。');
+      console.log(rows);
+    } catch (error) {
+      console.log('データベースへの登録中にエラーが発生しました:', error.message);
+      throw error; 
+    }
+  }
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+  
+try {
+    const name: string = req.body.name;
+
+    const megaInfo: MegaInfo = {  name};
+    await registerThread(megaInfo);
+        console.log('入力が完了しました。');
+        context.res = {
+        status: 200,
+        body: "ok"
+        };
+    } catch (error) {
+      console.log('エラーが発生しました:', error.message);
+      context.res = {
+        status: 500,
+        body: "Internal Server Error"
+      };
+    }
 };
-
-export default httpTrigger;
