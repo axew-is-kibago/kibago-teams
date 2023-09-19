1817c1b69db86d2287325ae0726199f9cfff78bc
rif_c <102364395+rif-c@users.noreply.github.com>
2023-09-19 13:36:46 +0000

    ハンバーガーメニューの実装

--- src/pages/EventCreate.tsx
+++ src/pages/EventCreate.tsx
@@ -1,6 +1,8 @@
+import Header from '../components/Header'
 export const EventCreate = () => {
     return (
       <>
+      <Header />
         <div className="text-center text-xl py-[5vh]">イベント作成 (あとで置き換え)</div>
         <div id="wrapper" className="w-2/3 ml-auto mr-auto border-2 rounded mb-[8vh]">
         <div id="input-wrapper" className="w-3/4 ml-auto mr-auto pb-[8vh]">
