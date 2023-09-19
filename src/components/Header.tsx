9fa8457381728f4fc5a28890736761a246ff5cbd
nakashimashin <nakashimashin0517@gmail.com>
2023-09-19 16:56:26 +0900

    headerコンポーネント作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,13 @@
 function Header() {
     return (
       <>    
-        <div className="navbar bg-base-100">
-        <div className="flex-none">
-            <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-            </button>
-        </div>
+        <div className="navbar bg-info">
         <div className="flex-1">
-            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            <a className="btn btn-ghost normal-case text-xl">タイトル</a>
         </div>
         <div className="flex-none">
             <button className="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-10 h-10 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
             </button>
         </div>
         </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
-import Button from "../components/button";
+import Button from "../components/Button";
+import Header from "../components/Header";
 
 export const Home = () => {
     return (
       <>
+        <Header />
         <div className="">Home</div>
         <Button />
       </>
