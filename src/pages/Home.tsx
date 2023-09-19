3f28022648bae9590702569b0a2b20cb7520c2f7
nakashimashin <nakashimashin0517@gmail.com>
2023-09-19 20:28:30 +0900

    テキストボックス作成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,16 @@
 import Button from "../components/Button";
 import Header from "../components/Header";
+import EventBox from "../components/EventBox";
 
 export const Home = () => {
     return (
       <>
         <Header />
-        <div className="">Home</div>
-        <div className="ml-5">
-        <Button />
+        <div className="flex flex-col justify-center items-center">
+          <div className="">Home</div>
+          <EventBox />
         </div>
+        <Button />
       </>
     );
   };
