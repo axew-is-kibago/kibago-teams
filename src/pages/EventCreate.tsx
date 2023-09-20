ce086279cbb5bbfddaea33e2bd38b5cbcbeeaeba
rif_c <102364395+rif-c@users.noreply.github.com>
2023-09-20 07:58:57 +0000

    Event関連について、複数表示に対応した

--- src/pages/EventCreate.tsx
+++ src/pages/EventCreate.tsx
@@ -4,8 +4,8 @@ export const EventCreate = () => {
       <>
       <Header />
         <div className="text-center text-xl py-[5vh]">イベント作成</div>
-        <div id="wrapper" className="w-2/3 ml-auto mr-auto border-2 rounded mb-[8vh]">
-        <div id="input-wrapper" className="w-3/4 ml-auto mr-auto pb-[8vh]">
+        <div className="w-2/3 ml-auto mr-auto border-2 rounded mb-[8vh]">
+        <div className="w-3/4 ml-auto mr-auto pb-[8vh]">
         <form className="pt-[6vh]"><p className="pb-[2vh]">イベント名<span className="text-red-500 required-dot">*</span></p><input className="border-2 rounded w-full h-[34px]"/></form>
         <form className="pt-[6vh]"><p className="pb-[2vh]">場所<span className="text-red-500 required-dot">*</span></p><input className="border-2 rounded w-full h-[34px]"/></form>
         <form className="pt-[6vh]"><p className="pb-[2vh]">日程<span className="text-red-500 required-dot">*</span></p><input className="border-2 rounded w-full h-[34px]"/></form>
--- src/pages/MyEventInfo.tsx
+++ src/pages/MyEventInfo.tsx
@@ -1,16 +1,48 @@
 import Button from "../components/Button";
 import Header from "../components/Header";
 import MyEventBox from "../components/MyEventBox";
+import { useState, useEffect } from 'react';
+
+type MyEventInfo = {
+  id:number;
+  title:string;
+  representative:string;
+  location:string;
+  date:string;
+  overview:string;
+}
 
 export const MyEventInfo = () => {
+  const [myEventArray, setMyEventArray] = useState<MyEventInfo[]>([
+    {id: 1, title: "パン食い競争", representative:"eat_bread", location: "どこかの小学校", date:"01月23日", overview:"ノーマルなイベントです"},
+    {id: 2, title: "二重跳びギネス記録チャレンジ", representative:"challenger", location: "スカイツリーの頂点", date:"02月13日", overview:"ぎりぎりノーマルなイベントです"},
+    {id: 3, title: "人間の限界を超えろ", representative:"space", location: "飛行機の上", date:"11月11日", overview:"おかしいイベントです"},
+  ])
+
+  useEffect(() => { console.log("出力テスト"); })
+
+  const handleClick = () => {
+    setMyEventArray((prevState) => ([...prevState, {id: myEventArray.length+1, title: "ボディビル研究会" + String(myEventArray.length-2), representative:"中山", location: "ゴールド事務", date:"11月11日", overview:"目指せ筋肉"}]));
+    console.log(myEventArray);
+  }
+
+
     return (
       <>
         <Header />
-        <div className="pt-[100px]">myevents</div>
-        <MyEventBox />
+        <div className="w-2/3 ml-auto mr-auto rounded mb-[8vh]">
+        <div className="w-3/4 ml-auto mr-auto pb-[8vh]">
+        <div className="pt-[100px] text-center">myevents</div>
+        <div className="text-center mt-5"><button onClick={handleClick} className='text-center btn'>追加</button></div>
+        <div className='w-screen flex flex-col justify-center items-center mt-10'></div>
+          {myEventArray.map((myevent: MyEventInfo) => {
+            return(
+              <MyEventBox key={myevent.id} title={myevent.title} representative={myevent.representative} location={myevent.location} date={myevent.date} overview={myevent.overview}/>
+            )
+          })}
+        </div>
+        </div>
         <Button href = "/home" title = "戻る"/>
       </>
     );
   };
-
-
