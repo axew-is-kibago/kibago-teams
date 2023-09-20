059697c271aa503282a04165a5bcc78ccf5b8700
nakashimashin <nakashimashin0517@gmail.com>
2023-09-20 15:12:13 +0900

    myeventにheader追加

--- src/pages/MyEventInfo.tsx
+++ src/pages/MyEventInfo.tsx
@@ -1,9 +1,11 @@
 import Button from "../components/Button";
+import Header from "../components/Header";
 
 export const MyEventInfo = () => {
     return (
       <>
-        <div>information</div>
+        <Header />
+        <div className="pt-[100px]">myevents</div>
         <Button href = "/home" title = "戻る"/>
       </>
     );
