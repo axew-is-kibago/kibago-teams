78e6d21d68d3a122804c1f0f3900aba1563bfc64
YuSa0-6 <fuku.sugar@icloud.com>
2023-09-21 02:11:20 +0900

    addProfile Trigger not able to jp lang

---
+++ api/src/functions/addProfile.ts
@@ -0,0 +1,27 @@
+import { app, HttpRequest, HttpResponseInit, InvocationContext, output } from "@azure/functions";
+
+const sqlOutput = output.generic({
+    type: 'sql',
+    commandtext: 'User_profile',
+    connectionStringSetting: 'SqlConnectionString'
+})
+
+export async function addProfile(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    const product = await request.json()
+    product.header("Content-Type", "application/json; charset=utf-8");
+    context.log("😛: ", product)
+    context.extraOutputs.set(sqlOutput, product);
+
+    return {
+        status: 201,
+        body: JSON.stringify(product)
+    };
+
+}
+
+app.http('addProfile', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    extraOutputs: [sqlOutput],
+    handler: addProfile
+});
