df38108e2e51bde8a3d7e8fd12cc2a1207f07b99
nakashimashin <nakashimashin0517@gmail.com>
2023-09-20 13:18:50 +0900

    コンポーネントの位置を調整

--- src/components/EventBox.tsx
+++ src/components/EventBox.tsx
@@ -10,16 +10,16 @@ type EventInfo ={
 function EventBox({title, representative, location, date, overview}: EventInfo) {
     return (
       <>
-        <div className="flex flex-col border pl-3 border-black w-1/2 h-[400px] rounded">
+        <div className="flex flex-col border pl-3 border-black w-1/2 h-[400px] rounded mb-10">
             <div className="text-[40px]">{title}</div>
             <div className="flex flex-col space-y-1">
                 <div className="text-[20px]">代表者：{representative}</div>
                 <div className="text-[20px]">場所：{location}</div>
                 <div className="text-[20px]">日程：{date}</div>
             </div>
-            <label className="text-[20px]">概要:{overview}</label>
+            <label className="text-[20px]">概要</label>
             <div className="border border-black w-5/6 h-1/2 text-[20px]">
-            概要
+            {overview}
             </div>
         </div>
       </>
