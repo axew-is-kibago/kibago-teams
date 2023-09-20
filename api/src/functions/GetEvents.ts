3432d072f8e925bb204fd8bd6878e77383530975
YuSa0-6 <fuku.sugar@icloud.com>
2023-09-21 00:28:58 +0900

    fix

--- api/src/functions/GetEvents.ts
+++ api/src/functions/GetEvents.ts
@@ -1,9 +1,8 @@
 import { app, input } from '@azure/functions';
 
-// The input binding executes the `select * from Products where Cost = @Cost` query, returning the result as json object in the body.
-// The *parameters* argument passes the `{cost}` specified in the URL that triggers the function,
-// `getproducts/{cost}`, as the value of the `@Cost` parameter in the query.
-// *commandType* is set to `Text`, since the constructor argument of the binding is a raw query.
+/**
+ * イベントを取得する関数
+ */
 const sqlInput = input.generic({
     type: 'sql',
     commandText: 'select * from Events,Users where Events.fb_uid = Users.fb_uid',
