9b7e76a71a2f6e3f399ae789ac9db7918f56ab2c
Hanu NAM <143068120+EricNam9413@users.noreply.github.com>
2023-09-20 07:55:15 +0000

    profile rearrange

--- src/components/Userbox.tsx
+++ src/components/Userbox.tsx
@@ -1,14 +1,20 @@
 import { FaUser } from 'react-icons/fa'; // Font Awesome
 
-function ParticipantIcon() {
+type UserInfo = {
+  id: number;
+  name: string;
+};
+
+function ParticipantIcon({id, name}: UserInfo) {
   return (
     <div className="z-[30]">
-      <div className="flex items-center rounded border border-black p-4 mb-8">
+      <div className="flex items-center rounded border border-black mb-8 h-[100px]">
         <FaUser className="text-6xl mr-4" />
-        <div className="text-3xl">ユーザー名</div>
+        <div className="text-3xl">ID : {id}</div>
+        <div className="text-3xl">NAME : {name}</div>
+        </div>
       </div>
-    </div>
-  );
+      );
 }
 
 export default ParticipantIcon;
