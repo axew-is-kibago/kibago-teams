123d0c8c2358fda199a30f7214b5033da83e60ac
nakashimashin <nakashimashin0517@gmail.com>
2023-09-20 15:03:47 +0900

    イベントボックスコンポーネントスタイリング

--- src/components/EventBox.tsx
+++ src/components/EventBox.tsx
@@ -11,14 +11,14 @@ function EventBox({title, representative, location, date, overview}: EventInfo) 
     return (
       <>
         <div className="flex flex-col border pl-3 border-black w-1/2 h-[400px] rounded mb-10">
-            <div className="text-[40px]">{title}</div>
-            <div className="flex flex-col space-y-1">
+            <div className="text-[40px] font-bold">{title}</div>
+            <div className="flex flex-row space-x-5">
                 <div className="text-[20px]">代表者：{representative}</div>
                 <div className="text-[20px]">場所：{location}</div>
                 <div className="text-[20px]">日程：{date}</div>
             </div>
             <label className="text-[20px]">概要</label>
-            <div className="border border-black w-5/6 h-1/2 text-[20px]">
+            <div className="border border-black w-5/6 h-2/3 text-[20px]">
             {overview}
             </div>
             <div className="flex flex-row w-full justify-end space-x-5 pr-3">
