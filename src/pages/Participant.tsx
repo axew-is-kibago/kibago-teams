03d8c19cacb97f1efa3e3e1f6e09e2d9cfbd0e9d
nakashimashin <nakashimashin0517@gmail.com>
2023-09-21 14:58:07 +0900

    参加者一覧作成

--- src/pages/Participant.tsx
+++ src/pages/Participant.tsx
@@ -1,47 +1,72 @@
+import { useAuthContext } from '../context/authContext';
 import ParticipantIcon from "../components/Userbox";
 import Button from "../components/Button";
 import Header from "../components/Header";
 import { useState, useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 
 type BoxInfo = {
   id: number;
-  name: string;
+  fb_uid: string;
+  sex: string;
+  weight: number;
+  height: number;
+  target: string;
 };
 
 
 export const Participant = () => {
-  const [profileArray, setprofileArray] = useState<BoxInfo[]>([
-    { id: 1, name: "John" },
-    { id: 2, name: "Steve" },
-    { id: 3, name: "Bob" },
-    { id: 4, name: "June" },
-    { id: 5, name: "Pate" },
-  ])
+  const user = useAuthContext();
 
-  useEffect(() => { console.log("出力テスト"); })
+  const [profileArray, setProfileArray] = useState<BoxInfo[]>([]);
+  const [isLoading, setLoading] = useState(true);
+  const [isError, setError] = useState(false);
 
-  const handleClick = () => {
-    setprofileArray((prevState) => ([...prevState, {id: profileArray.length+1, name: "Jake"}]));
-    console.log(profileArray); }
+  useEffect(() => {
+    (async() => {
+      try {
+        const response = await fetch(`/api/GetProfile`);
+        const info = await response.json();
+        setProfileArray(() => ([...info ]));
+        console.log(profileArray);
+      }catch(err) {
+        setError(true);
+      }finally {
+        setLoading(false);
+      }
+    })()
+  }, []);
 
-  return (
-    <>
-      <Header />
-      <div className="flex flex-col items-center w-screen">
-        <div className="flex flex-col items-center w-full">
-          <div className="ml-5 w-full mt-10 flex flex-col items-center">
-            <div className="text-gray-700 py-[5vh]" style={{ fontSize: '50px' }}>
-              参加者一覧
-            </div>
-            <div className="w-1/2 text-center"><button onClick={handleClick} className='text-center btn'>追加</button>
-              {profileArray.map((box: BoxInfo) => {
-                return <ParticipantIcon id={box.id} name={box.name} />;
-              })}
+  if(isLoading) {
+    return <p>...loading</p>
+  }
+
+  if(isError){
+    return <p>Error!</p>
+  }
+
+  if(!user){
+    return <Navigate to="/signin"/>
+  } else{
+    return (
+      <>
+        <Header />
+        <div className="flex flex-col items-center w-screen">
+          <div className="flex flex-col items-center w-full">
+            <div className="ml-5 w-full mt-10 flex flex-col items-center">
+              <div className="text-gray-700 py-[5vh]" style={{ fontSize: '50px' }}>
+                参加者一覧
+              </div>
+              <div className="w-1/2 text-center">
+                {profileArray.map((box: BoxInfo) => {
+                  return <ParticipantIcon key={box.id} name={box.fb_uid} sex={box.sex} weight={box.weight} height={box.height} target={box.target} />;
+                })}
+              </div>
             </div>
           </div>
+          <Button href="/" title="戻る" />
         </div>
-        <Button href="/" title="戻る" />
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };  
